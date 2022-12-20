aa232cf1a6255b979de9617aaa836d290b6affa6
6altika <113434015+6altika@users.noreply.github.com>
2022-12-20 18:41:56 +0300

    Перенес шаблон

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Home from './components/Home'
+import Cart from './components/Cart'
 import Product from './components/Product'
 import {BrowserRouter, Routes, Route} from 'react-router-dom'
 
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home/>} exact />
           <Route path='/product' element={<Product/>} exact />
+          <Route path='/Cart' element={<Cart/>} exact />
         </Routes>
       </div>
     </BrowserRouter>
